cd704a0c97e3f86c5eb44caab756fe394afe0d01
Andrewgbr17 <161317183+Andrewgbr17@users.noreply.github.com>
2024-04-11 09:02:19 -0300

    Add files via upload

---
+++ a-soma_var_function.js
@@ -0,0 +1,12 @@
+const num1 = 25;
+const num2 = 25;
+const resultado = num1 + num2;
+
+///MESMA FUNCAO DE CODIGO - FAZEM A MEMSMA COISA
+
+function soma (num1, num2)
+{
+    return num1 + num2;
+}
+   console.log(soma(5, 5))
+   console.log(resultado)
