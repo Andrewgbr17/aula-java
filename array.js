169e1804f945c95e17d0e14e0c6a5e1f71e7eb0e
Andrewgbr17 <161317183+Andrewgbr17@users.noreply.github.com>
2024-04-16 12:20:24 -0300

    Add files via upload

---
+++ array.js
@@ -0,0 +1,7 @@
+//ARRAY
+
+const arrayVazia = [,,,];
+console.log(arrayVazia.length) // 3 posições
+arrayVazia.push(50)
+console.log(arrayVazia) // 3 vazios e agora o 50 (push)
+console.log(arrayVazia.length)  // 4 posições
