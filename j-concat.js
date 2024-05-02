d263e6e5a844842482db4979b2ebe69978aaf1e4
Andrewgbr17 <161317183+Andrewgbr17@users.noreply.github.com>
2024-05-02 07:55:01 -0300

    Add files via upload

---
+++ j-concat.js
@@ -0,0 +1,7 @@
+const salaJS = ["Evaldo", "Camis", "Mari"];
+const salaPython = ["Junior", "Leonardo", "Raquele"];
+
+//const salasUnificadas = salaPython.concat(salaJS); //MUDAR ORDEM
+const salasUnificadas = salaJS.concat(salaPython);
+
+console.log(salasUnificadas);
